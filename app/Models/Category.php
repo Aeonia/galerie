00a4a69c1bb6ea230ebacf0b74caa7e776e0d78d
Aeonia <anna.cathelineau@gmail.com>
2018-01-09 17:08:04 +0100

    first commit

---
+++ app/Models/Category.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Events\CategorySaving;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name', 'slug',
+    ];
+    public function images()
+        {
+            return $this->hasMany(Image::class);
+        }
+    protected $dispatchesEvents = [
+    'saving' => CategorySaving::class,
+];
+}
